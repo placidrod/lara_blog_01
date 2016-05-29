2be3315812208b6db0f12109c759da26e6aa3d18
Placid Rodrigues (ICT) <placid@ananta.com>
2016-05-29 10:12:18 +0600

    Corrected slug functionality and moved to AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Post;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Post::saving(function ($post)
+        {
+            $title = $post->title;
+            $proposed_slug = strtolower(str_replace(' ', '-', $title));
+            $existing_slug = Post::where('slug', $proposed_slug)->where('id', '<>', $post->id)->first();
+
+            if(! $existing_slug) {
+                $post->slug = $proposed_slug;
+            } else {
+                $number_added_to_slug = 0;
+
+                do {
+                    $number_added_to_slug += 1;
+                    $numbered_proposed_slug = $proposed_slug . '-' . $number_added_to_slug;
+                    $existing_slug = Post::where('slug', $numbered_proposed_slug)->where('id', '<>', $post->id)->first();
+                } while($existing_slug);
+
+                $post->slug = $numbered_proposed_slug;
+            }
+        });
     }
 
     /**
